5cac8a92d36e118a9def572eaee82dc8186a1ac0
Halil Coşgun <cosgun.halil@gmail.com>
2022-12-13 00:42:40 +0300

    feat(base): Add unity scene object

---
+++ Runtime/Base/VP_UnitySceneObject.cs
@@ -0,0 +1,11 @@
+namespace com.voxelpixel.hannibal_ui
+{
+    using UnityEngine;
+
+    public class VP_UnitySceneObject : MonoBehaviour
+    {
+        public virtual void PreInit() { }
+        public virtual void Init() { }
+        public virtual void LateInit() { }
+    }
+}
