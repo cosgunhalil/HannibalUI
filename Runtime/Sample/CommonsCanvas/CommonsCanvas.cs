2113e5c8c6e313a609841e1eed0d74a0914d9e37
Halil Coşgun <cosgun.halil@gmail.com>
2023-03-22 01:56:18 +0300

    feat(sample): Add menu buttons

--- Runtime/Sample/CommonsCanvas/CommonsCanvas.cs
+++ Runtime/Sample/CommonsCanvas/CommonsCanvas.cs
@@ -5,9 +5,26 @@ namespace com.voxelpixel.hannibal_ui.sample
 
     public class CommonsCanvas : VP_Canvas
     {
+        [SerializeField]
+        private VP_HorizontalRectTransformContainer _horizontalRectTransformContainer;
         public override void Setup()
         {
             Debug.Log("CommonsCanvas::Setup()");
+        }
+
+        public override void LateInit()
+        {
+            if (_horizontalRectTransformContainer == null) 
+            {
+#if UNITY_EDITOR
+                Debug.LogError("_horizontalRectTransformContainer is null!");
+#endif
+                return;
+            }
+
+            _horizontalRectTransformContainer.Configure(new Vector2(canvasSize.x, canvasSize.y * .25f), 
+                new Padding() { Top = 0f, Bottom = 0f, Left = 1.0f, Right = 1.0f }, 
+                5);
         }
     }
 }
