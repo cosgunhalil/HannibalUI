b2fe0e71e2d7b6d655e27c482427d262a960eeed
Halil Coşgun <cosgun.halil@gmail.com>
2023-03-22 01:08:06 +0300

    feat(ui): Add commons canvas script

---
+++ Runtime/Sample/CommonsCanvas/CommonsCanvas.cs
@@ -0,0 +1,13 @@
+namespace com.voxelpixel.hannibal_ui.sample 
+{
+    using com.voxelpixel.hannibal_ui.base_component;
+    using UnityEngine;
+
+    public class CommonsCanvas : VP_Canvas
+    {
+        public override void Setup()
+        {
+            Debug.Log("CommonsCanvas::Setup()");
+        }
+    }
+}
