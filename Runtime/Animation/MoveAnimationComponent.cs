3f833f3b24ac67e06bfb73feaaa6d54d2a05d355
Halil Coşgun <cosgun.halil@gmail.com>
2026-07-01 22:51:26 +0300

    feat(package): Remove unnecessary assembly definitions

--- Runtime/Animation/MoveAnimationComponent.cs
+++ Runtime/Animation/MoveAnimationComponent.cs
@@ -1,7 +1,8 @@
+using DG.Tweening;
+
 namespace com.voxelpixel.hannibal_ui.animation
 {
     using UnityEngine;
-    using DG.Tweening;
     public class MoveAnimationComponent : AnimationComponent, IAnimable
     {
         public Vector2 DeactivatedCoordinate { get; set; }
@@ -15,13 +16,11 @@ namespace com.voxelpixel.hannibal_ui.animation
 
         public void PlayForward(float animationTime)
         {
-            //TODO: use do anchor position!
             ObjectRectTransform.DOAnchorPos(ActivatedCoordinate, animationTime).SetEase(Ease.InOutSine);
         }
 
         public void PlayRewind(float animationTime)
         {
-            //TODO: use do anchor position!
             ObjectRectTransform.DOAnchorPos(DeactivatedCoordinate, animationTime).SetEase(Ease.InOutSine);
         }
     }
--- Sample/CommonsCanvas/CharactersButton.cs
+++
@@ -1,9 +0,0 @@
-namespace com.voxelpixel.hannibal_ui.sample 
-{
-    using HannibalUI.Runtime.Base;
-
-    public class CharactersButton : VP_Button
-    {
-        
-    }
-}
